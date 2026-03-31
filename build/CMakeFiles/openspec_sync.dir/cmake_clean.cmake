a446784cb07e62b6d202023651e740f347bb9ea3
RobsonRojas <robson.rojas@gmail.com>
2026-03-31 14:15:57 -0300

    fix: remove build folder

--- build/CMakeFiles/openspec_sync.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/openspec_sync"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/openspec_sync.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/openspec_sync.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for openspec_sync.
-# This may be replaced when dependencies are built.
